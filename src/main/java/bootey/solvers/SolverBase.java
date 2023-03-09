0393dafd79cf2fb4794df0e06255f37198509745
gravagnani <giuseppe.ravagnani@gmail.com>
2023-03-09 18:06:48 +0100

    pushj

--- src/main/java/bootey/solvers/SolverBase.java
+++ src/main/java/bootey/solvers/SolverBase.java
@@ -1,6 +1,13 @@
 package bootey.solvers;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.javatuples.Pair;
+
 import bootey.dto.ChallengeModel;
+import bootey.dto.Snake;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -15,7 +22,23 @@ public class SolverBase implements Solver {
          * in caso di warmhole conto 0
          */
 
-        challenge.getSnakeList();
+        challenge.getSnakeList().sort(Comparator.comparing(Snake::getLen).reversed());
+
+        for (Snake s : challenge.getSnakeList()) {
+            int[] coord = getMaxScoreCoords(challenge.getMatrix());
+            s.getActions().add("" + coord[0]);
+            s.getActions().add("" + coord[1]);
+            challenge.getMatrix()[coord[0]][coord[1]] = Integer.MIN_VALUE;
+            for (int i = 1; i < s.getLen(); i++) {
+                Pair<Integer, Integer> c = getNextMove(coord, challenge.getMatrix());
+                // TODO: conta quanti punti facciamo
+
+            }
+
+        }
+
+        // NON TOCCARE!
+        challenge.getSnakeList().sort(Comparator.comparing(Snake::getId));
 
         log.info("Solved");
     }
@@ -23,6 +46,60 @@ public class SolverBase implements Solver {
     public static Integer scoreCalculator() {
 
         return null;
+    }
+
+    public static int[] getMaxScoreCoords(Integer[][] matrix) {
+
+        int maxVal = Integer.MIN_VALUE;
+        int[] maxIndex = new int[2];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == null)
+                    continue;
+                if (matrix[i][j] > maxVal) {
+                    maxVal = matrix[i][j];
+                    maxIndex[0] = i;
+                    maxIndex[1] = j;
+                }
+            }
+        }
+
+        return maxIndex;
+
+    }
+
+    public static Pair<Integer, Integer> getNextMove(int[] coord, Integer[][] matrix) {
+        int x = coord[0];
+        int y = coord[1];
+
+        int width = matrix.length;
+        int height = matrix[0].length;
+
+        List<Pair<Integer, Integer>> coords = new ArrayList<>();
+
+        coords.add(new Pair<>((x - 1 + width) % width, y));
+        coords.add(new Pair<>((x + 1 + width) % width, y));
+        coords.add(new Pair<>(x, (y - 1 + height) % height));
+        coords.add(new Pair<>(x, (y + 1 + height) % height));
+
+        int max = Integer.MAX_VALUE;
+        Pair<Integer, Integer> maxCoords = null;
+        for (Pair<Integer, Integer> c : coords) {
+            Integer val = matrix[c.getValue0()][c.getValue1()];
+            if (val == null) {
+                val = 0;
+            }
+            if (val > max) {
+                max = val;
+                maxCoords = c;
+            }
+        }
+
+        if (maxCoords == null)
+            throw new RuntimeException();
+
+        return maxCoords;
     }
 
 }

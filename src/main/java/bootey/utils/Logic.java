9cca77fd4368f759e4cbaa0677dd29acd3e230c3
icalzavara <i.calzavara@reply.it>
2024-03-21 17:59:20 +0100

    fix function with class

--- src/main/java/bootey/utils/Logic.java
+++ src/main/java/bootey/utils/Logic.java
@@ -2,28 +2,28 @@ package bootey.utils;
 
 import bootey.dto.ChallengeModel;
 import bootey.dto.GoldenPoint;
-import bootey.dto.SilverPoint;
+import bootey.dto.Point;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Logic {
 
-    public GoldenPoint getClosesGoldenPoint(ChallengeModel challenge, SilverPoint sp){
-        GoldenPoint closesGoldenPoint=null;
-        long lowestDistance=10000000000000l;
+    public Point getClosesGoldenPoint(ChallengeModel challenge, Point sp) {
+        Point closesGoldenPoint = null;
+        long lowestDistance = 10000000000000l;
         for (GoldenPoint gp : challenge.getGoldenPoints()) {
-                int distance = getDistance(sp,gp);
-                if (distance<lowestDistance) {
-                    closesGoldenPoint=gp;
-                }
+            int distance = getDistance(sp, gp);
+            if (distance < lowestDistance) {
+                closesGoldenPoint = gp;
+            }
 
         }
         return closesGoldenPoint;
     }
 
-    private int getDistance(SilverPoint sp, GoldenPoint gp) {
-        return Math.abs(sp.getX()-gp.getX())+Math.abs(sp.getY()-gp.getY());
+    private int getDistance(Point sp, Point gp) {
+        return Math.abs(sp.getX() - gp.getX()) + Math.abs(sp.getY() - gp.getY());
     }
 
 }
